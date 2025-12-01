927d59b41a33bf9c05ed9d70f111ef5d21fadcaa
Carlos Onocuica <carloscoderr@gmail.com>
2025-12-01 00:29:38 -0500

    Construcción del menu y estilo de botones definidos V1

---
+++ Racoca-DSWI/Controllers/RegistroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Racoca_DSWI.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult TipoDeUsuario()
+        {
+            return View();
+        }
+    }
+}
